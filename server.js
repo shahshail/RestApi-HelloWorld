f7cc84443a59d04bfbf11735fed29bcbbc62871e
Shail Shah <shahshail9898@gmail.com>
2018-05-27 23:33:05 -0400

    Custome dynamic port allocation

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ var express = require('express')
 var app = express()
 
 //Port number that we are gonna listning on
+//Rather than hardcore the port number we can implement a method to get argument from command line and set the port numbers dynamically..
+var program_name = process.argv[0];
+var script_path = process.argv[1];
+var port_string = process.argv[2];
+
 var port_number = "9999"; // tells express to listen this port
+if(typeof port_string !== "undefined" && port_string.length > 0){
+    console.log("port_string=" + port_string);
+
+    port_number = port_string;
+}
 
 app.listen(Number(port_number));
 console.log('Server is running....')
